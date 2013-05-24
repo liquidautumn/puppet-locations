f7a48cc9d38e4c9dac87329b46fd4b9315af01b5
Denis Grankin <liquidautumn@gmail.com>
2013-05-24 23:39:42 +0300

    added bash script for apt based linuxes to reinstall puppet and specify master name

---
+++ bash/puppet-reset.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Expect puppet master name as first parameter, agent name obtained from host name.
+
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' puppet|grep "install ok installed")
+
+echo Checking for puppet: $PKG_OK
+
+if [ "" == "$PKG_OK" ]; then
+  echo "No puppet installed, no removal required."
+else
+  sudo apt-get --force-yes --yes --purge remove puppet
+fi
+
+sudo apt-get --force-yes --yes install puppet
+
+sudo chmod a+w /etc/puppet/puppet.conf
+
+sudo cat >> /etc/puppet/puppet.conf << EOF
+[agent]
+    certname = `hostname --fqdn`
+    server = ${1-puppet}
+EOF
+
+sudo chmod a-w /etc/puppet/puppet.conf
